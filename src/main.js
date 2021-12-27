6fd935db90e076c66055d1865ac364d9df6e13ca
James Futhey <kidGodzilla@users.noreply.github.com>
2021-12-26 17:50:41 -0800

    New vue-3 project (vite), bootstrap, some templates and styles

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootswatch/dist/zephyr/bootstrap.css'
+
+createApp(App).mount('#app')
