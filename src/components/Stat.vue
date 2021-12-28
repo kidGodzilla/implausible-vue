325fcdd657eda4d441782383b15a97554943a9c9
James Futhey <kidGodzilla@users.noreply.github.com>
2021-12-27 22:22:51 -0800

    Update Stat.vue

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -24,53 +24,53 @@ function fmtMSS(s) { return(s-(s%=60))/60+('m ')+parseInt(s) }
 
 async function getValue() {
   const whereClause = whereClauseComponents(store);
-  console.log('whereClause', whereClause);
+  // console.log('whereClause', whereClause);
 
   if (props.title === 'Visitors') {
     const sql = `SELECT count(DISTINCT ip) from visits${whereClause}`;
-    console.log(sql);
+    // console.log(sql);
 
     let result = await query(sql);
-    console.log(result, result[0]['count(DISTINCT ip)']);
+    // console.log(result, result[0]['count(DISTINCT ip)']);
     stat.value = result[0]['count(DISTINCT ip)'];
 
   } else if (props.title === 'Total Pageviews') {
     const sql = `SELECT count(*) from VISITS${whereClause}`;
-    console.log(sql);
+    // console.log(sql);
 
     let result = await query(sql);
-    console.log(result, result[0]['count(*)']);
+    // console.log(result, result[0]['count(*)']);
 
     stat.value = result[0]['count(*)'];
 
   } else if (props.title === 'Bounce Rate') {
     let sql = `SELECT count(DISTINCT ip) from visits${whereClause}`;
-    console.log(sql);
+    // console.log(sql);
 
     let result = await query(sql);
-    console.log(result, result[0]['count(DISTINCT ip)']);
+    // console.log(result, result[0]['count(DISTINCT ip)']);
 
     let visitorCount = result[0]['count(DISTINCT ip)'];
 
     sql = `SELECT count(DISTINCT ip) from visits${whereClause} AND is_new = 1`;
-    console.log(sql);
+    // console.log(sql);
 
     result = await query(sql);
-    console.log(result, result[0]['count(DISTINCT ip)']);
+    // console.log(result, result[0]['count(DISTINCT ip)']);
 
     let onePageVisits = result[0]['count(DISTINCT ip)'];
 
-    console.log('onePageVisits / visitorCount', onePageVisits / visitorCount, onePageVisits, visitorCount)
+    // console.log('onePageVisits / visitorCount', onePageVisits / visitorCount, onePageVisits, visitorCount)
 
     stat.value = visitorCount === 0 ? 0 : percentFormatter(onePageVisits / visitorCount);
 
 
   } else if (props.title === 'Session Length') {
     const sql = `SELECT AVG(x.MaxValue) FROM (SELECT ip, max(session_length) as MaxValue from visits${whereClause} GROUP BY ip) x`;
-    console.log(sql);
+    // console.log(sql);
 
     let result = await query(sql);
-    console.log(result, result[0]['AVG(x.MaxValue)']);
+    // console.log(result, result[0]['AVG(x.MaxValue)']);
 
     stat.value = fmtMSS(result[0]['AVG(x.MaxValue)']) || 0;
 
