eb3d5158d2cfd0162ddcd3ded2396b284d5bdd81
James Futhey <kidGodzilla@users.noreply.github.com>
2021-12-28 15:39:41 -0800

    top level await not supported

--- src/store/query-utils.js
+++ src/store/query-utils.js
@@ -8,19 +8,33 @@ const wasmUrl = new URL("../../node_modules/sql.js-httpvfs/dist/sql-wasm.wasm", 
 // import store from "../store";
 // console.log('query-utils store', store);
 
-const worker = await createDbWorker([{
-        from: "inline",
-        config: {
-            serverMode: "full",
-            url: `${ location.protocol }//${ location.host }/analytics.sqlite3`,
-            requestChunkSize: 1024
-        }
-    }],
-    workerUrl.toString(),
-    wasmUrl.toString()
-);
+// let url = (location.hostname === 'localhost' ? `${ location.protocol }//${ location.host }` : `${ location.protocol }//analytics.serv.rs`) + `/analytics.sqlite3`;
+let url = `${ location.protocol }//${ location.hostname === 'localhost' ? location.host : 'analytics.serv.rs' }/analytics.sqlite3`;
+
+async function init() {
+    if (window._worker) return window._worker;
+
+    async function createWorker() {
+        const worker = await createDbWorker([{
+                from: "inline",
+                config: {
+                    serverMode: "full",
+                    url: url || `${ location.protocol }//${ location.host }/analytics.sqlite3`,
+                    requestChunkSize: 1024
+                }
+            }],
+            workerUrl.toString(),
+            wasmUrl.toString()
+        );
+        return worker;
+    }
+
+    window._worker = await createWorker();
+    return window._worker;
+}
 
 export async function query(string) {
+    let worker = await init();
     return await worker.db.query(string);
 }
 
