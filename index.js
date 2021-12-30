ea57a42415964d98ea13bebd1989f92d74adc552
James Futhey <kidGodzilla@users.noreply.github.com>
2021-12-29 18:45:17 -0800

    update sqlite every hour

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const download = require('download');
+const schedule = require('node-schedule');
+
+function downloadSqlite() {
+    download('https://analytics.serv.rs/analytics.sqlite3?i='+(Math.floor(Math.random() * 9999999)), `${__dirname}/public`).then(() => {
+        console.log('Downloaded analytics.sqlite3');
+    });
+}
+
+schedule.scheduleJob('30 * * * *', downloadSqlite);
+downloadSqlite();
 
 let debug = process.env.DEBUG || false;
 const PORT = process.env.PORT || 5001;
