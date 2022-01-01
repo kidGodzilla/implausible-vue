fdd39c22041ab187269fcb71c3669b3a5dc33f95
James Futhey <kidGodzilla@users.noreply.github.com>
2022-01-01 14:59:58 -0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-center">
+  <p class="text-center mt-3">
     Copyright &copy; <span class="year">2021</span> <a href="https://jamesfuthey.com/" target="_blank">James Futhey</a>.
     All Rights Reserved.
   </p>
