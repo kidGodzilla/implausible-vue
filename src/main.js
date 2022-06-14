4f1fefc4e6aa84798ca93479775cdddb1464b1b3
James Futhey <kidGodzilla@users.noreply.github.com>
2022-06-13 21:56:03 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import { createApp } from 'vue'
 import store from './store'
 import App from './App.vue'
 
-const app = createApp(App).use(store).mount('#app')
+const app = createApp(App).use(store).mount('#app');
